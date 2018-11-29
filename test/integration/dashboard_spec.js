35dcf335315add1012a6ec9c14782c0cd5f47a01
Santiago Ferreira <san650@gmail.com>
2018-11-29 15:25:01 -0300

    Add open issues count to dashboard

--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,2 +1,20 @@
 describe('Dashboard', function() {
+    it('shows the number of open issues', function() {
+        cy.fixture({
+            title: "Issue 1",
+            status: "open"
+        });
+        cy.fixture({
+            title: "Issue 2",
+            status: "open"
+        });
+        cy.fixture({
+            title: "Issue 3",
+            status: "closed"
+        });
+
+        cy.visit('/dashboard');
+
+        cy.contains('2').should('exist');
+    });
 });
