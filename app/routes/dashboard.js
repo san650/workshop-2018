6983ea85db568036c830f853a3e6e6a9156001d3
Santiago Ferreira <san650@gmail.com>
2018-11-30 11:19:28 -0300

    Add high severity gauge to dashboard

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,9 +4,18 @@ const { issueStore } = require('../models');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  const openIssuesCount = issueStore.getAllOpen().length;
+  const openIssues = issueStore.getAllOpen();
+  const openIssuesCount = openIssues.length;
+  let highIssuesCount = 0;
+  if (openIssuesCount > 0) {
+    highIssuesCount =
+      openIssues.filter(issue => issue.severity === 'High').length / openIssuesCount;
+  }
 
-  res.render('dashboard', {openIssuesCount});
+  res.render('dashboard', {
+    openIssuesCount,
+    highIssuesCount
+  });
 });
 
 module.exports = router;
